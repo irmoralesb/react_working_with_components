c4df75039a4081ff3404ebbcc53c2964dff24b4d
Raul Morales <irmoralesb@hotmail.com>
2025-07-19 08:14:59 -0600

    Encapsulating Theme into ThemeContext

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useContext } from 'react';
-import { ThemeContext } from '../../App'
+import { ThemeContext } from '../../contexts/ThemeContext'
 
 const Header = ({layoutVersion }) => {
 
